93014f8d2ff740eb2ae36aa3fccc35fb8e36f1ca
aja531 <aja531@gmail.com>
2020-06-14 10:50:48 -0600

    Initial Files

---
+++ server1/test6.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# start client iperf session on port 5000
+        iperf3 -c 10.3.0.100 -p 5000 -t 84600 -b 100 -i 1 -f m &>/dev/null &
+
+done
+
---
+++ server6/server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Starts server iperf3 session on port 5000
+#
+iperf3 -s -p 5000 >/dev/null &
+
+done
+
